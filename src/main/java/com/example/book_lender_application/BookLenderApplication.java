26be83c9d8c928f727785b1703cbea7b3ef30571
Amin Ranjbar <m.amin.ranjbar@gmail.com>
2023-01-30 21:28:59 +0100

    Initial commit

---
+++ src/main/java/com/example/book_lender_application/BookLenderApplication.java
@@ -0,0 +1,13 @@
+package com.example.book_lender_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookLenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/book_lender_application/BookLenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.book_lender_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
